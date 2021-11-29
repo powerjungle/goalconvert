855d7b9b862683e009801c93afa467bbccc35fe4
powerjungle <fabsidereal@gmail.com>
2021-11-29 23:49:29 +0100

    better gui design

--- alconvgui/alconvert-gui.go
+++ alconvgui/alconvert-gui.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"image/color"
+	"strconv"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
-
-	"fmt"
-	"strconv"
 
 	"github.com/powerjungle/goalconvert/alconvert"
 )
@@ -78,11 +80,9 @@ func fromInputToAlcv(label string, alcoval *alconvert.Alcovalues, changeTo float
 	}
 }
 
-func newInputWidgets(label string,
-	rangeMin float64,
-	rangeMax float64,
-	alcoval *alconvert.Alcovalues,
-	aLa *allLabels) *inputWidgets {
+func newInputWidgets(label string, rangeMin float64, rangeMax float64,
+	alcoval *alconvert.Alcovalues, aLa *allLabels) *inputWidgets {
+
 	iw := inputWidgets{
 		amountLabel:  widget.NewLabel(label),
 		amountSlider: widget.NewSlider(rangeMin, rangeMax),
@@ -102,13 +102,13 @@ func newInputWidgets(label string,
 	iw.amountEntry.OnChanged = func(inputStr string) {
 		if f, err := strconv.ParseFloat(inputStr, 32); err == nil {
 			fromInputToAlcv(label, alcoval, f)
-		} else {
+		} else if inputStr != "" {
 			fmt.Println(err)
 		}
 
 		if f, err := strconv.ParseFloat(inputStr, 64); err == nil {
 			iw.amountSlider.SetValue(f)
-		} else {
+		} else if inputStr != "" {
 			fmt.Println(err)
 		}
 	}
@@ -116,15 +116,82 @@ func newInputWidgets(label string,
 	return &iw
 }
 
+func makeIOCanvasObjects(iWs1 *inputWidgets, iWs2 *inputWidgets,
+	calcValueLabel1 *widget.Label, calcValueLabel2 *widget.Label,
+	calculatedLabel string, firstResultLabel string, secondResultLabel string) []fyne.CanvasObject {
+
+	label1 := widget.NewLabel(firstResultLabel)
+	label2 := widget.NewLabel(secondResultLabel)
+	label2spacer := layout.NewSpacer()
+
+	if firstResultLabel == "nope" {
+		label1.Hide()
+	}
+
+	if secondResultLabel == "nope" {
+		label2.Hide()
+		label2spacer.Hide()
+	}
+
+	co := []fyne.CanvasObject{}
+
+	line := canvas.NewLine(color.White)
+
+	cL := widget.NewLabel(calculatedLabel)
+	cL.TextStyle.Bold = true
+	co = append(co, cL)
+
+	if iWs1 != nil {
+		co = append(co, iWs1.amountLabel, iWs1.amountEntry, iWs1.amountSlider)
+	}
+
+	if iWs2 != nil {
+		co = append(co, iWs2.amountLabel, iWs2.amountEntry, iWs2.amountSlider)
+	}
+
+	cont1 := container.NewHBox(label1, label2spacer, label2)
+
+	cont2obj := []fyne.CanvasObject{}
+
+	if calcValueLabel1 != nil {
+		calcValueLabel1.TextStyle.Bold = true
+		cont2obj = append(cont2obj, calcValueLabel1)
+	}
+
+	cont2obj = append(cont2obj, label2spacer)
+
+	if calcValueLabel2 != nil {
+		calcValueLabel2.TextStyle.Bold = true
+		cont2obj = append(cont2obj, calcValueLabel2)
+	}
+
+	cont2 := container.NewHBox(cont2obj...)
+
+	co = append(co, cont1, cont2, line)
+
+	return co
+}
+
+func compileCanvasObjects(objs ...[]fyne.CanvasObject) []fyne.CanvasObject {
+	allObjs := []fyne.CanvasObject{}
+	for _, v := range objs {
+		for _, v2 := range v {
+			allObjs = append(allObjs, v2)
+		}
+	}
+	return allObjs
+}
+
 func main() {
 	av := alconvert.NewAV()
 
 	alcApp := app.New()
 	alcWindow := alcApp.NewWindow("goalconvert")
-	alcWindow.Resize(fyne.NewSize(300, 0))
+	alcWindow.Resize(fyne.NewSize(600, 600))
 
 	aLa := initAllLabels()
 
+	// Init input widgets
 	mlInputWidgets := newInputWidgets("Milliliters", 0, 2000, av, aLa)
 
 	percInputWidgets := newInputWidgets("Percentage", 0, 100, av, aLa)
@@ -132,64 +199,64 @@ func main() {
 	unitTarInputWidgets := newInputWidgets("Unit Target", 0, 20, av, aLa)
 	unitTarInputWidgets.amountSlider.Step = 0.1
 
-	percTarInputWidgets := newInputWidgets("Percentage Target", 0, 20, av, aLa)
+	percTarInputWidgets := newInputWidgets("Percentage Target", 0, 100, av, aLa)
 
 	mlTarInputWidgets := newInputWidgets("Milliliter Target", 0, 2000, av, aLa)
+	///////////////////////
 
-	alcWindow.SetContent(
-		container.NewVBox(
-			mlInputWidgets.amountLabel,
-			mlInputWidgets.amountEntry,
-			mlInputWidgets.amountSlider,
-			percInputWidgets.amountLabel,
-			percInputWidgets.amountEntry,
-			percInputWidgets.amountSlider,
-			widget.NewLabel("Calculated Units using ml and %"),
-			aLa.unitsLabel,
-			unitTarInputWidgets.amountLabel,
-			unitTarInputWidgets.amountEntry,
-			unitTarInputWidgets.amountSlider,
-			widget.NewLabel("Calculated Unit Target"),
-			container.NewHBox(
-				widget.NewLabel("ml for target units"),
-				layout.NewSpacer(),
-				widget.NewLabel("removed ml for target"),
-			),
-			container.NewHBox(
-				aLa.finalMlLabel,
-				layout.NewSpacer(),
-				aLa.finalRemAmLabel,
-			),
-			percTarInputWidgets.amountLabel,
-			percTarInputWidgets.amountEntry,
-			percTarInputWidgets.amountSlider,
-			widget.NewLabel("Calculated Percent Target"),
-			container.NewHBox(
-				widget.NewLabel("Amount of Water To Add"),
-				layout.NewSpacer(),
-				widget.NewLabel("Total Amount Left"),
-			),
-			container.NewHBox(
-				aLa.finalTarPerc,
-				layout.NewSpacer(),
-				aLa.finalTarPercA,
-			),
-			mlTarInputWidgets.amountLabel,
-			mlTarInputWidgets.amountEntry,
-			mlTarInputWidgets.amountSlider,
-			widget.NewLabel("Calculated Percent Target"),
-			container.NewHBox(
-				widget.NewLabel("After Adding Water (%)"),
-				layout.NewSpacer(),
-				widget.NewLabel("Difference between ml"),
-			),
-			container.NewHBox(
-				aLa.finalTarMlP,
-				layout.NewSpacer(),
-				aLa.finalTarMlD,
-			),
-		),
+	// Init input CanvasObjects
+	gotUnitsObjects := makeIOCanvasObjects(
+		mlInputWidgets,
+		percInputWidgets,
+		aLa.unitsLabel,
+		nil,
+		"Calculated Units",
+		"Calculated Units using ml and %",
+		"nope",
 	)
+
+	gotUnitTargetObjects := makeIOCanvasObjects(
+		unitTarInputWidgets,
+		nil,
+		aLa.finalMlLabel,
+		aLa.finalRemAmLabel,
+		"Calculated Unit Target",
+		"ml for target units",
+		"removed ml for target",
+	)
+
+	gotPercTargetObjects := makeIOCanvasObjects(
+		percTarInputWidgets,
+		nil,
+		aLa.finalTarPerc,
+		aLa.finalTarPercA,
+		"Calculated Percent Target",
+		"Amount of Water To Add",
+		"Total Amount Left",
+	)
+
+	gotMlTargetObjects := makeIOCanvasObjects(
+		mlTarInputWidgets,
+		nil,
+		aLa.finalTarMlP,
+		aLa.finalTarMlD,
+		"Calculated Milliliter Target",
+		"After Adding Water (%)",
+		"Difference between ml",
+	)
+	///////////////////////
+
+	allObjs := compileCanvasObjects(
+		gotUnitsObjects,
+		gotUnitTargetObjects,
+		gotPercTargetObjects,
+		gotMlTargetObjects,
+	)
+
+	makeContainer := container.NewVBox(allObjs...)
+	scrollCont := container.NewVScroll(makeContainer)
+
+	alcWindow.SetContent(scrollCont)
 
 	alcWindow.ShowAndRun()
 }
